c96b7248eb1effc3f84947a64e314c782a43f0cf
Zenoviia <tkachenko.zenoviia@lll.kpi.ua>
2025-04-01 09:51:52 +0300

    Add length operation
    This operation determines the length of a list based on its dimensions.

--- array_based_list.js
+++ array_based_list.js
@@ -11,8 +11,31 @@ class LinkedList {
   getSize() {
     return this.size;
   }
+
+  getLength() {
+    return this.size;
+  }
+
+  add(value) {
+    this.list.push(value);
+    this.size++;
+  }
+
+  printList() {
+    console.log("List:", this.list);
+  }
 }
 
 const list = new LinkedList();
+
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+
+console.log("List after adding elements:");
+list.printList();
+
 console.log("List empty:", list.isEmpty());
-console.log("List size:", list.getSize());
+console.log("List size after additions:", list.getSize());
+console.log("List length after additions:", list.getLength());
